a2892e5b56eef05b9b03c5ec90d51737f5261b01
nomtaek <54359476+nomtaek@users.noreply.github.com>
2024-03-24 13:14:21 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-83f19c2d-c5fe-4dc7-ade6-ca7838a9c464/)
 
 
 echo "$curl_output" >> Update.tsx
